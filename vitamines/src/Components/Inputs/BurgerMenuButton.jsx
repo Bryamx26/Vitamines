3f28aa9b48fda25dc79eae93b91f547a7862c555
Bryam Carlo Rishing Silva <144278205+Bryamx26@users.noreply.github.com>
2025-07-08 00:40:00 +0200

    Update BurgerMenuButton.jsx

--- vitamines/src/Components/Inputs/BurgerMenuButton.jsx
+++ vitamines/src/Components/Inputs/BurgerMenuButton.jsx
@@ -10,7 +10,7 @@ function BurgerMenuButton({isOpen, setIsOpen }) {
     return (
         <img
             className="burgerMenu"
-            src={isOpen ? "/public/images/close.png"  : "/public/images/burgerMenu.png"}
+            src={isOpen ? "/images/close.png"  : "/public/images/burgerMenu.png"}
             alt="bouton menu burger"
             onClick={handleToggle}
         />
