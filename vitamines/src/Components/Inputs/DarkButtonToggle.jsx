c6e2d90b65cbfac08a10d003a075e849d294c1bb
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2025-07-07 21:30:02 +0200

    Update DarkButtonToggle.jsx

--- vitamines/src/Components/Inputs/DarkButtonToggle.jsx
+++ vitamines/src/Components/Inputs/DarkButtonToggle.jsx
@@ -2,6 +2,7 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext.jsx";
 
+
 function DarkButtonToggle() {
     const { isDark, setIsDark } = useContext(ThemeContext);
 
