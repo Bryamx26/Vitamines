c7758de02aa9f82594d3944da046b5cb0de6733c
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2025-07-24 20:37:06 +0200

    correction dee l'affichage du texte en mode mobile

--- vitamines/src/Components/Inputs/SearchInput.jsx
+++ vitamines/src/Components/Inputs/SearchInput.jsx
@@ -9,6 +9,7 @@ function SearchInput({ value, onChange }) {
                     onChange={onChange}
                     placeholder="Rechercher"
                     id="search-input"
+                    autoComplete="off"
 
                 />
                 <label className={"inputSearchLabelImage"}  htmlFor={"search-input"}><img src={"/images/search.svg"} alt="Search" style={{ width: "15px" , height: "15px" }} /></label>
